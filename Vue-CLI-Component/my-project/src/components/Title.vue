ff69e9c9f2e4bc097392184a525bb9f70e944600
Süleyman <sduymaz8@gmail.com>
2023-06-12 16:16:36 +0300

    12.06.23

---
+++ Vue-CLI-Component/my-project/src/components/Title.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>{{Title}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            Title:"Vue 3 Component"
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
