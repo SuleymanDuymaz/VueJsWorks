c611be68ef5b26ed2740e269aa8310df3c4e7a84
Süleyman <sduymaz8@gmail.com>
2023-06-23 16:20:58 +0300

    add

--- Planner/planner/src/views/Home.vue
+++ Planner/planner/src/views/Home.vue
@@ -1,33 +1,34 @@
 <template>
   <div class="home">
-  
     <div v-for="project in projects" :key="project.id">
-      <p>{{project.title}}</p>
-
-   
-    
-
+     <single-project :project="project" @delete="handleDelete"></single-project>
    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import SingleProject from '../components/SingleProject.vue';
 
 export default {
   name: 'Home',
-data() {
-  return {
-    projects: []
-  }
-},
-mounted() {
-  fetch('http://localhost:8000/projects')
-  .then(res=>res.json())
-  .then(data=>this.projects=data)
-  .catch(err=>console.log(err.message))
-},
-
+  components: {
+    SingleProject
+  },
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+    .then(res => res.json())
+    .then(data => this.projects = data)
+    .catch(err => console.log(err.message));
+  },
+  methods: {
+    handleDelete(id){
+      this.projects=this.projects.filter((project)=>project.id!==id)
+    }
+  },
 };
 </script>
