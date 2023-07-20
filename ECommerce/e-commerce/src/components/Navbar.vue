283f44c3b64849a1600cbc638703f281a87eec72
Süleyman <sduymaz8@gmail.com>
2023-07-20 14:19:44 +0300

    style

--- ECommerce/e-commerce/src/components/Navbar.vue
+++ ECommerce/e-commerce/src/components/Navbar.vue
@@ -47,5 +47,12 @@ export default {
 </script>
 
 <style>
-
+.v-toolbar__title {
+  font-size: 1rem !important;
+}
+.v-badge__badge {
+  font-size: 10px !important;
+  height: 18px !important;
+  min-width: 18px !important;
+}
 </style>
