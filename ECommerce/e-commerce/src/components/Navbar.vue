7f5655ce67912c7545867b6c709b452b0ff263cd
Süleyman <sduymaz8@gmail.com>
2023-07-20 12:47:01 +0300

    navbar.vue

---
+++ ECommerce/e-commerce/src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+ <v-app-bar app color="#385308" dark>
+    <v-btn icon>
+        <v-icon>
+            mdi-magnify
+        </v-icon>
+
+    </v-btn>
+
+
+ </v-app-bar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ ECommerce/e-commerce/src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <HelloWorld></HelloWorld>
+    <Navbar class="index"/>
+    <Dashboard/>
+    <Footer />
+  </div>
+</template>
+
+<script>
+  import HelloWorld from '../components/HelloWorld'
+  import Navbar from '../components/Navbar'
+  import Dashboard from '../components/Dashboard.vue'
+  import Footer from '../components/Footer.vue'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      HelloWorld,
+      Navbar,
+      Dashboard,
+      Footer
+    },
+  }
+</script>
+
+<style scoped>
+.index{
+  z-index: 9999;
+}
+</style>
