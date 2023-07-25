d0b282b3783568917b027a212e76fc112deedadc
Süleyman <sduymaz8@gmail.com>
2023-07-25 15:05:49 +0300

    ..

--- ECommerce/e-commerce/src/components/Dashboard.vue
+++ ECommerce/e-commerce/src/components/Dashboard.vue
@@ -8,23 +8,18 @@
       class="d-flex align-center justify-center"
       dark
     >
-    <v-row>
-      <v-col cols="12" sm="12">
-        <h4 class="text-center">Mens LifeStyle</h4>
-      </v-col>
-      <v-col cols="12" sm="12">
-        <v-breadcrumbs :items="items" class="justify-center mt-n7" dark>
-          <template v-slot:divider >
-            <v-icon color="#7C92FE">
-              mdi-chevron-right
-            </v-icon>
-
-          </template>
-
-        </v-breadcrumbs>
-
-      </v-col>
-    </v-row>
+      <v-row>
+        <v-col cols="12" sm="12">
+          <h4 class="text-center">MEN'S LIFESTYLE SHOES</h4>
+        </v-col>
+        <v-col cols="12" sm="12">
+          <v-breadcrumbs :items="items" class="justify-center mt-n7" dark>
+            <template v-slot:divider>
+              <v-icon color="#7C92FE">mdi-chevron-right</v-icon>
+            </template>
+          </v-breadcrumbs>
+        </v-col>
+      </v-row>
     </v-card>
     <v-card tile class="mx-16 mt-n10 card1" color="white">
       <v-row>
@@ -32,33 +27,34 @@
           <v-card height="250px" tile flat color="#3890D8">
             <v-row>
               <v-col cols="12" sm="4" class="pl-8 pt-5">
-                <v-btn color="black" dark class="withoutupercase" small tile>New</v-btn>
+                <v-btn color="black" dark class="withoutupercase" small tile>
+                  new
+                </v-btn>
                 <h6 class="white--text mt-8">MOUNTAIN BIKE</h6>
-               <h3 class="white--text mt-5">Trek Speed Concept</h3>
+                <h3 class="white--text mt-5">Trek Speed Concept</h3>
                 <v-btn
                   rounded
                   color="#4C9CDB"
                   dark
                   class="withoutupercase px-8 mt-10"
                 >
-                More Info 
-
+                  More Info
                 </v-btn>
               </v-col>
-             <v-col cols="12" sm="8">
+              <v-col cols="12" sm="8">
                 <v-img
                   src="bike.png"
                   max-height="400"
-                  max-width="350"
+                  max-width="400"
                   contain
                   class="mt-n6"
                 ></v-img>
-              </v-col>       
+              </v-col>
             </v-row>
           </v-card>
         </v-col>
         <v-col cols="12" sm="4" class="pl-0">
-           <v-card height="250px" tile flat color="#6857E1">
+          <v-card height="250px" tile flat color="#6857E1">
             <v-row>
               <v-col cols="12" sm="6" class="pl-8 pt-5">
                 <v-btn color="black" dark class="withoutupercase" small tile>
@@ -75,7 +71,7 @@
                   More Info
                 </v-btn>
               </v-col>
-           <v-col cols="12" sm="6">
+              <v-col cols="12" sm="6">
                 <v-img
                   src="jacket2.png"
                   max-height="400"
@@ -86,39 +82,33 @@
               </v-col>
             </v-row>
           </v-card>
-
         </v-col>
-
       </v-row>
       <v-row>
         <v-col cols="12" sm="3" class="mt-n6 pr-0">
-          <v-toolbaar flat outlined>
-            <v-toolbar-title><strong> Filter</strong> </v-toolbar-title>
+          <v-toolbar flat outlined>
+            <v-toolbar-title><strong>Filter</strong></v-toolbar-title>
             <v-spacer></v-spacer>
             <v-divider vertical></v-divider>
             <v-btn icon class="ml-1">
-              <v-icon color="3853D8">
-                mdi-sync
-              </v-icon>
+              <v-icon color="#3853D8"> mdi-sync </v-icon>
             </v-btn>
-            
-          </v-toolbaar>
+          </v-toolbar>
         </v-col>
-        <v-col cols="12" sm="3" class="mt-n6 pr-0">
+        <v-col cols="12" sm="3" class="mt-n6 px-0">
           <v-toolbar flat outlined>
             <v-toolbar-title>
-              SPORT BY: <span>Price $$</span>
+              SPORT BY: <span>Price $-$$</span>
             </v-toolbar-title>
             <v-spacer></v-spacer>
             <v-btn icon class="mr-4">
               <v-icon color="black">mdi-menu-down</v-icon>
             </v-btn>
-
           </v-toolbar>
         </v-col>
         <v-col cols="12" sm="3" class="mt-n6 px-0">
           <v-toolbar flat outlined class="">
-            <v-toolbar-title>SHOW:<span>12</span></v-toolbar-title>
+            <v-toolbar-title>SHOW: <span>12</span></v-toolbar-title>
             <v-spacer></v-spacer>
             <v-btn icon class="mr-4">
               <v-icon color="black">mdi-menu-down</v-icon>
@@ -128,71 +118,77 @@
         <v-col cols="12" sm="3" class="mt-n6 pl-0">
           <v-toolbar flat outlined>
             <v-btn icon class="mr-1">
-              <v-icon color="#3853D8">mdi-apps </v-icon>
+              <v-icon color="#3853D8">mdi-apps</v-icon>
             </v-btn>
             <v-divider vertical></v-divider>
-            <v-btn  icon class="mx-1">
+            <v-btn icon class="mx-1">
               <v-icon color="grey">mdi-format-list-bulleted</v-icon>
             </v-btn>
-             <v-divider vertical></v-divider>
-            <v-toolbar-title>
-              <strong>
-                COMPARE :
-              </strong>
+            <v-divider vertical></v-divider>
+            <v-toolbar-title class="ml-2">
+              <strong>COMPARE :</strong>
             </v-toolbar-title>
             <v-spacer></v-spacer>
             <v-badge color="#3853D8" content="3" class="mr-2"></v-badge>
           </v-toolbar>
         </v-col>
-           <v-col cols="3" class="py-0 pr-0 mt-n3">
-              <v-card flat outlined tile>
-                <v-toolbar flat>
-                  <v-icon color="black" class="mr-2">
-                    mdi-chevron-down
-                  </v-icon>
-                  <v-strong>CATEGORIES</v-strong>
-                  <v-spacer></v-spacer>
-                  <v-icon color="grey" small>mdi-close</v-icon>
-                </v-toolbar>
+        <v-col cols="3" class="py-0 pr-0 mt-n3">
+          <v-card flat outlined tile>
+            <v-toolbar flat>
+              <v-icon color="black" class="mr-2">mdi-chevron-down</v-icon>
+              <strong>CATEGORIES</strong>
+              <v-spacer></v-spacer>
+              <v-icon color="grey" small>mdi-close</v-icon>
+            </v-toolbar>
             <v-list dense class="mt-n5">
-             <v-list-item v-for="style in styles" :key="style.title">
-               <v-list-item-content>
-                    <v-list-item-title
+              <v-list-item v-for="style in styles" :key="style.title">
+                <v-list-item-content>
+                  <v-list-item-title
                     v-text="style.title"
                     class="ml-8"
                   ></v-list-item-title>
-              </v-list-item-content>
+                </v-list-item-content>
                 <v-list-item-action>
                   <v-list-item-subtitle
                     v-text="style.count"
                   ></v-list-item-subtitle>
                 </v-list-item-action>
-  </v-list-item>
-</v-list>
-
-              </v-card>
-              <v-card flat outlined tile>
-                <v-toolbar flat>
-                  <v-icon color="black" class="mr-2">msi-chevron-down</v-icon>
-                  <stron>PRICE</stron>
-                  <v-spacer></v-spacer>
-                  <v-icon color="grey" small>mdi-close</v-icon>
-                </v-toolbar>
-                <v-toolbar  flat>
-                  <v-text-field placeholder="$50" filled rounded dense class="mx-2"></v-text-field>
-                  <v-text-field placeholder="$1900" filled rounded dense class="mx-2"></v-text-field>
-                </v-toolbar>
-                <v-range-slider color="blue" max="40" min="30"></v-range-slider>
-
-              </v-card>
-              <v-card flat outlined tile>
-                <v-toolbar flat>
-                  <v-icon color="black" class="mr-2">mdi-chevron-down</v-icon>
-                  <strong>BRAND</strong>
-                  <v-spacer></v-spacer>
-                  <v-icon color="grey" small>mdi-close</v-icon>
-                </v-toolbar>
-                           <v-list dense class="mt-n5">
+              </v-list-item>
+            </v-list>
+          </v-card>
+          <v-card flat outlined tile>
+            <v-toolbar flat>
+              <v-icon color="black" class="mr-2">msi-chevron-down</v-icon>
+              <strong>PRICE</strong>
+              <v-spacer></v-spacer>
+              <v-icon color="grey" small>mdi-close</v-icon>
+            </v-toolbar>
+            <v-toolbar flat>
+              <v-text-field
+                placeholder="$50"
+                filled
+                rounded
+                dense
+                class="mx-2"
+              ></v-text-field>
+              <v-text-field
+                placeholder="$1900"
+                filled
+                rounded
+                dense
+                class="mx-2"
+              ></v-text-field>
+            </v-toolbar>
+            <v-range-slider color="blue" max="40" min="-30"></v-range-slider>
+          </v-card>
+          <v-card flat outlined tile>
+            <v-toolbar flat>
+              <v-icon color="black" class="mr-2">mdi-chevron-down</v-icon>
+              <strong>BRAND</strong>
+              <v-spacer></v-spacer>
+              <v-icon color="grey" small>mdi-close</v-icon>
+            </v-toolbar>
+            <v-list dense class="mt-n5">
               <v-list-item v-for="brand in brands" :key="brand.title">
                 <v-list-item-content>
                   <v-list-item-title
@@ -208,16 +204,15 @@
                 </v-list-item-action>
               </v-list-item>
             </v-list>
-           </v-card>
-           <v-card flat outlined tile>
+          </v-card>
+          <v-card flat outlined tile>
             <v-toolbar flat>
               <v-icon color="black" class="mr-2">mdi-chevron-down</v-icon>
-              <stron>COLOR</stron>
+              <strong>COLOR</strong>
               <v-spacer></v-spacer>
               <v-icon color="grey" small>mdi-close</v-icon>
-
             </v-toolbar>
-              <v-chip-group column multiple>
+            <v-chip-group column multiple>
               <v-chip
                 filter
                 outlined
@@ -239,9 +234,8 @@
               <v-chip filter color="black" text-color="white"> </v-chip>
               <v-chip filter color="lime" text-color="white"> </v-chip>
             </v-chip-group>
-
-           </v-card>
-            <v-card flat outlined>
+          </v-card>
+          <v-card flat outlined>
             <v-toolbar flat>
               <v-icon color="black" class="mr-2">mdi-chevron-down</v-icon>
               <strong>SIZE</strong>
@@ -264,8 +258,8 @@
               </v-chip>
             </v-chip-group>
           </v-card>
-           </v-col>
-          <v-col cols="9" class="mt-n3">
+        </v-col>
+        <v-col cols="9" class="mt-n3">
           <v-row>
             <v-col
               cols="12"
@@ -291,11 +285,96 @@
                       clothe.title
                     }}</strong>
                   </v-card-text>
-                 
+                  <v-expand-transition>
+                    <div
+                      v-if="hover"
+                      class="
+                        d-flex
+                        transition-fast-in-fast-out
+                        blue
+                        v-card--reveal
+                        display-3
+                        white--text
+                      "
+                      style="height: 100%"
+                    >
+                      <v-btn rounded color="white">
+                        <v-icon>mdi-cart-outlined</v-icon>
+                        Buy
+                      </v-btn>
+                      <v-btn fab small color="white" class="ml-2">
+                        <v-icon color="black">mdi-content-copy</v-icon>
+                      </v-btn>
+                      <v-btn fab small color="white" class="ml-2">
+                        <v-icon color="black">mdi-heart-outline</v-icon>
+                      </v-btn>
+                    </div>
+                  </v-expand-transition>
                 </v-card>
               </v-hover>
             </v-col>
           </v-row>
+        </v-col>
+        <v-col cols="12" sm="3" class="mt-n3 pr-0 pb-0">
+          <v-toolbar flat outlined> </v-toolbar>
+        </v-col>
+        <v-col cols="12" sm="3" class="mt-n3 px-0 pb-0">
+          <v-toolbar flat outlined>
+            <v-btn icon class="mr-1">
+              <v-icon color="grey">mdi-chevron-up</v-icon>
+            </v-btn>
+            <v-divider vertical></v-divider>
+          </v-toolbar>
+        </v-col>
+        <v-col cols="12" sm="3" class="mt-n3 px-0 pb-0">
+          <v-toolbar flat outlined class="text-center">
+            <v-pagination v-model="page" :length="4"></v-pagination>
+          </v-toolbar>
+        </v-col>
+        <v-col cols="12" sm="3" class="mt-n3 pl-0 pb-0">
+          <v-toolbar flat outlined>
+            <v-btn icon class="mr-1">
+              <v-icon color="grey">mdi-chevron-down</v-icon>
+            </v-btn>
+            <v-divider vertical></v-divider>
+          </v-toolbar>
+        </v-col>
+      </v-row>
+    </v-card>
+    <v-card
+      color="#EFF0F2"
+      height="250px"
+      tile
+      flat
+      dark
+      class="d-flex align-center justify-center mt-n10 card2"
+    >
+      <v-row>
+        <v-col cols="12" sm="12">
+          <h4 class="text-center black--text mt-10">
+            SUBSCRIBE TO OUR NEWSLETTER
+          </h4>
+        </v-col>
+        <v-col cols="12" sm="12">
+          <h6 class="text-center grey--text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+            dolorem dignissimos <br />
+            Eos eligendi cupiditate cumque nemo sapiente quae omnis doloribus
+            ducimus.
+          </h6>
+        </v-col>
+        <v-col cols="12" sm="12">
+          <v-card flat color="transparent" class="d-flex justify-center">
+            <v-text-field
+              placeholder="Enter Your Email"
+              rounded
+              class="shrink"
+              background-color="grey"
+              filled
+              append-icon="mdi-email-outline"
+              dense
+            ></v-text-field>
+          </v-card>
         </v-col>
       </v-row>
     </v-card>
